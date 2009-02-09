5d551eb2f1f502940d94e9c2d5a06607eff9ea17
Tyler Montgomery <tyler.a.montgomery@gmail.com>
2009-02-09 12:58:31 -0700

    lighting executable update

--- spec/parsing_a_feed.rb
+++ spec/parsing_a_feed.rb
@@ -2,19 +2,21 @@ require File.join(File.dirname(__FILE__), %w[spec_helper])
 
 describe Lightning::Parser do
   
-  it "should parse an entire feed" do
-      posts = Lightning::Parser.parse(:feed=>"#{Dir.pwd}/spec/test_feed.xml")
-      posts.should_not be_nil
+  before(:each) do
+    @posts = Lightning::Parser.parse(:feed=>"#{Dir.pwd}/spec/test_feed.xml")
+  end
+  
+  it "should parse an entire feed into Lighting::Post objects" do
+      @posts.should_not be_nil
+       @posts.first.class.should == Lightning::Post
   end
   
   it "should parse part of a feed" do
-     post = Lightning::Parser.parse(:feed=>"http://awesomerails.wordpress.com/feed/", :posts=>1)
+     post = Lightning::Parser.parse(:feed=>"#{Dir.pwd}/spec/test_feed.xml", :posts=>1)
      post.should_not be_nil
   end
+
   
-  it "should return Lightning::Post objects" do
-      posts = Lightning::Parser.parse(:feed=>"http://awesomerails.wordpress.com/feed/")
-      posts.first.class.should == Lightning::Post
-  end
+
     
 end
